60e99c6e06e937af5c823367d7002dbe6bf98c00
CloudSen <www.402645063@gmail.com>
2019-07-22 02:58:16 +0800

    refactor(component, snackbar): snack bar V1.0

--- src/components/Main/DynamicSnackBar.vue
+++ src/components/Main/DynamicSnackBar.vue
@@ -1,23 +1,19 @@
 <template>
   <fragment>
-    <template v-for="(snack, index) in getAllSnackMessage">
+    <template v-for="snack in getAllSnackMessageWithStyle">
       <v-snackbar
-        :absolute="absoluteSnack"
-        :bottom="snackY === 'bottom'"
         :color="snack.color"
         :key="snack.id"
-        :left="snack.x === 'left'"
         :multi-line="isMultiLine"
-        :right="snack.x === 'right'"
-        :style="adjustSnackY(index)"
-        :timeout="snack.color === 'success' ? snack.timeout : 0"
-        :top="snackY === 'top'"
+        :style="snack.cssStyle"
+        :timeout="snack.color === 'error' ? 0 : snack.timeout"
+        bottom
         style="z-index: 10;"
         v-model="snack.show"
       >
         {{ snack.message }}
         <v-btn
-          @click="snack.show = false"
+          @click="onClose(snack.id)"
           flat
         >Shut Up</v-btn>
       </v-snackbar>
@@ -26,40 +22,23 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 
 export default {
   name: 'dynamic-snackbar',
-  props: {
-    snackList: {
-      type: Array,
-      required: true,
-    },
-  },
-  data () {
-    return {
-      snacks: this.snackList,
-    }
-  },
+  data: () => ({
+
+  }),
   computed: {
     isMultiLine () {
       return this.$vuetify.breakpoint.xsOnly
     },
-    snackY () {
-      return this.$vuetify.breakpoint.xsOnly ? 'bottom' : 'top'
-    },
-    absoluteSnack () {
-      return !this.$vuetify.breakpoint.xsOnly
-    },
-    ...mapState(['getAllSnackMessage']),
+    ...mapGetters(['getAllSnackMessageWithStyle', 'getMessageById']),
   },
   methods: {
-    adjustSnackY (snackIndex) {
-      const cssStyle = {}
-      if (this.snackList.length > 1) {
-        cssStyle.marginTop = `${(snackIndex) * 60}px`
-      }
-      return cssStyle
+    ...mapMutations(['deleteSnackMessage', 'hideSnackById']),
+    onClose (id) {
+      this.deleteSnackMessage({ id })
     },
   },
 }
