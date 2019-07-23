d8fd7ce4bdc0272c47874d02c7ffc2449c273fe3
CloudSen <www.402645063@gmail.com>
2019-07-23 08:40:26 +0800

    fix(app): fix font cdn add scroll style
    - 修复Hack font的cdn，之前填错了
    - 给浏览器滚动条添加自定义样式

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
 
 <style>
 /* 添加Hack字体CDN */
-@import "https://cdnjs.cloudflare.com/ajax/libs/hack-font/3.003/web/fonts/hack-bold-subset.woff";
+@import "//cdn.jsdelivr.net/npm/hack-font@3.3.0/build/web/hack.css";
 * {
   padding: 0;
   margin: 0;
@@ -30,5 +30,22 @@ code {
 
 nav {
   z-index: 999 !important;
+}
+
+/* 滚动条样式 */
+body::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #000;
+}
+
+body::-webkit-scrollbar {
+  width: 10px;
+  background-color: #000;
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: #20c20e;
+  border: 2px solid #20c20e;
 }
 </style>
