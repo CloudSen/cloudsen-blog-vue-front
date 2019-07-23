40a792cd7b6d63486b1a38b89f0cb8bea5087d1b
CloudSen <www.402645063@gmail.com>
2019-07-23 08:40:54 +0800

    refactor(views): delete markdown import

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,22 +9,15 @@
       rows="10"
       v-model="content"
     />
-    <markdown-it-vue
-      :content="content"
-      :options="options"
-      class="md-body"
-    />
   </v-container>
 </template>
 
 <script>
-import MarkdownItVue from 'markdown-it-vue'
 import MarkdownText from '@/components/temp-data/testMarkDown'
-import 'markdown-it-vue/dist/markdown-it-vue.css'
 
 export default {
   components: {
-    MarkdownItVue,
+
   },
   data () {
     return {
