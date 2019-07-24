4c57fbbf6d58fac67abc231fe61b7c9e0ba2f773
CloudSen <www.402645063@gmail.com>
2019-07-25 01:32:04 +0800

    feat(component): add to top button

---
+++ src/components/Main/RightSideButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-fab-transition>
+    <v-btn
+      @click.stop="scrollToTop"
+      bottom
+      color="blue darken-3"
+      dark
+      fab
+      fixed
+      right
+      ripple
+      small
+      transition="slide-x-reverse-transition"
+      v-scroll="onScroll"
+      v-show="showButton"
+    >
+      <v-icon dark>keyboard_arrow_up</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+export default {
+  name: 'right-side-btn',
+  data: () => ({
+    showButton: false,
+  }),
+  methods: {
+    scrollToTop () {
+      this.$vuetify.goTo(0)
+    },
+    onScroll (e) {
+      const top = window.pageYOffset || e.target.scrollTop || 0
+      this.showButton = top > 100
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
