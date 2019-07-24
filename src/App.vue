8853639d941a1215ae15e2a8f7eb9d79e3bccbf4
CloudSen <www.402645063@gmail.com>
2019-07-25 01:31:20 +0800

    style(app): change font and scroll style
    - 修改浏览器滚动条样式
    - 修改字体设置，全网默认宋体和Hack

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,23 @@ export default {
 * {
   padding: 0;
   margin: 0;
-  font: 14px/1.5 Arial, "\5b8b\4f53", "Microsoft Yahei", "Hiragino Sans GB",
-    "Heiti SC", "WenQuanYi Micro Hei", sans-serif;
+  font-size: 14px/1.5;
+  font-family: "\5b8b\4f53", Arial, "WenQuanYi Micro Hei", "Microsoft Yahei",
+    "Hiragino Sans GB", "Heiti SC", sans-serif;
 }
 
 html {
   overflow-y: auto;
 }
 
+pre,
 code {
   font: 14px/1.5 Hack, Consolas, Monaco, Menlo;
+}
+
+.headline {
+  font-family: "\5b8b\4f53", Arial, "Microsoft Yahei", "Hiragino Sans GB",
+    "Heiti SC", "WenQuanYi Micro Hei", sans-serif !important;
 }
 
 /* 滚动条样式 */
@@ -36,12 +43,28 @@ body::-webkit-scrollbar-track {
 }
 
 body::-webkit-scrollbar {
-  width: 10px;
+  width: 5px;
   background-color: #000;
 }
 
 body::-webkit-scrollbar-thumb {
   background-color: #20c20e;
   border: 2px solid #20c20e;
+}
+
+pre::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #000;
+}
+
+pre::-webkit-scrollbar {
+  height: 3px;
+  background-color: #000;
+}
+
+pre::-webkit-scrollbar-thumb {
+  background-color: #20c20e;
+  border: 1px solid #20c20e;
 }
 </style>
