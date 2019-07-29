a4c61fca6dc07acd3b2825866d85cc14f37684c3
CloudSen <www.402645063@gmail.com>
2019-07-29 23:31:27 +0800

    test(temp-data): add temp data for articles

---
+++ src/components/temp-data/articles.js
@@ -0,0 +1,138 @@
+export default [
+  {
+    id: '1',
+    title: 'Test1',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/12/20/FD6b60.md.png',
+    commentCount: 5,
+    readCount: 49,
+    tags: [
+      {
+        id: '1',
+        name: 'JavaScript',
+        color: 'amber lighten-2',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+  {
+    id: '2',
+    title: 'Test2',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/11/28/FVus3R.md.png',
+    commentCount: 2,
+    readCount: 23,
+    tags: [
+      {
+        id: '1',
+        name: 'java',
+        color: 'blue lighten-1',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+  {
+    id: '3',
+    title: 'Test1',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/12/20/FD6b60.md.png',
+    commentCount: 5,
+    readCount: 49,
+    tags: [
+      {
+        id: '1',
+        name: 'JavaScript',
+        color: 'amber lighten-2',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+  {
+    id: '4',
+    title: 'Test2',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/11/28/FVus3R.md.png',
+    commentCount: 2,
+    readCount: 23,
+    tags: [
+      {
+        id: '1',
+        name: 'java',
+        color: 'blue lighten-1',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+  {
+    id: '5',
+    title: 'Test1',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/12/20/FD6b60.md.png',
+    commentCount: 5,
+    readCount: 49,
+    tags: [
+      {
+        id: '1',
+        name: 'JavaScript',
+        color: 'amber lighten-2',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+  {
+    id: '6',
+    title: 'Test2',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/11/28/FVus3R.md.png',
+    commentCount: 2,
+    readCount: 23,
+    tags: [
+      {
+        id: '1',
+        name: 'java',
+        color: 'blue lighten-1',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+  {
+    id: '7',
+    title: 'Test1',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/12/20/FD6b60.md.png',
+    commentCount: 5,
+    readCount: 49,
+    tags: [
+      {
+        id: '1',
+        name: 'JavaScript',
+        color: 'amber lighten-2',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+  {
+    id: '8',
+    title: 'Test2',
+    summary: 'test summary sadfa ldfjaljof alkeflaj aslfj ljsdlf ',
+    img_url_md: 'https://s1.ax1x.com/2018/11/28/FVus3R.md.png',
+    commentCount: 2,
+    readCount: 23,
+    tags: [
+      {
+        id: '1',
+        name: 'java',
+        color: 'blue lighten-1',
+      },
+    ],
+    createTime: '2019-07-29',
+    updateTime: '2019-07-29',
+  },
+]
