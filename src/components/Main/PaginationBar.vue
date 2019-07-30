642f52ea189651238bc89bcc9b3ce51fd514af1b
CloudSen <www.402645063@gmail.com>
2019-07-30 19:56:22 +0800

    feat(component, view): add pagination bar

---
+++ src/components/Main/PaginationBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container
+    flat
+    fluid
+    grid-list-xs
+  >
+    <v-layout
+      justify-center
+      row
+      wrap
+    >
+      <v-flex shrink>
+        <v-pagination
+          :length="pageable.pageSize"
+          @input="onPageChange"
+          @next="onNext"
+          @previous="onPrevious"
+          circle
+          total-visible="5"
+          v-model="pageable.pageNumber"
+        ></v-pagination>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'paginationBar',
+  props: {
+    pageable: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    onNext () {},
+    onPrevious () {},
+    onPageChange () {},
+  },
+}
+</script>
+
+<style>
+</style>
--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,18 +1,27 @@
 <template>
-  <ArticleList :cards="cards"></ArticleList>
+  <div>
+    <ArticleList :cards="cards"></ArticleList>
+    <PaginationBar :pageable="pageable"></PaginationBar>
+  </div>
 </template>
 
 <script>
 import ArticleList from '@/components/Main/Blog/ArticleList'
 import ArticleSourceData from '@/components/temp-data/articles'
+import PaginationBar from '@/components/Main/PaginationBar'
 
 export default {
   name: 'articles',
   components: {
     ArticleList,
+    PaginationBar,
   },
   data: () => ({
     cards: ArticleSourceData,
+    pageable: {
+      pageSize: 20,
+      pageNumber: 2,
+    },
   }),
 }
 </script>
