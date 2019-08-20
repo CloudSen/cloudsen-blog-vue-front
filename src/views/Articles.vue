8ba0a6550b105638e571ae2b909cbb85d0aa99d4
CloudSen <www.402645063@gmail.com>
2019-08-21 01:59:25 +0800

    refactor(component, vue): add article api call

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <ArticleList :cards="cards"></ArticleList>
-    <PaginationBar :pageable="pageable"></PaginationBar>
+    <PaginationBar :page="page"></PaginationBar>
   </div>
 </template>
 
 <script>
 import ArticleList from '@/components/Main/Blog/ArticleList'
-import ArticleSourceData from '@/components/temp-data/articles'
+// import ArticleSourceData from '@/components/temp-data/articles'
 import PaginationBar from '@/components/Main/PaginationBar'
+import api from '@/api/api'
 
 export default {
   name: 'articles',
@@ -16,13 +17,32 @@ export default {
     ArticleList,
     PaginationBar,
   },
-  data: () => ({
-    cards: ArticleSourceData,
-    pageable: {
-      pageSize: 20,
-      pageNumber: 2,
+  data () {
+    return {
+      cards: [],
+      page: {
+        size: 20,
+        current: 1,
+        total: 1,
+        pages: 1,
+      },
+    }
+  },
+  methods: {
+    fetchArticleSummaryData () {
+      api.articleApi.pageArticleSummaryByCondition(this.page)
+        .then((response) => {
+          console.log(response.data)
+          this.cards = response.data.records
+        })
+        .catch((error) => {
+          console.error(error)
+        })
     },
-  }),
+  },
+  created () {
+    this.fetchArticleSummaryData()
+  },
 }
 </script>
 
